f7d58905958ce3150d6808e89c1aa14e00fcbebd
KanishkSogani <kanishksogani1@gmail.com>
2025-02-03 19:55:41 +0530

    add shadcn library for ui and initiated signup page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,29 +1,59 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { useDebounceValue } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+function SignIn() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debouncedUsername = useDebounceValue(username, 500);
+  const { toast } = useToast();
+  const router = useRouter();
 
-export default function Component() {
-  const { data: session, status } = useSession();
+  //ZOD IMPLEMENTATION
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-  if (status === "loading") {
-    return null; // or a loading spinner
-  }
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage(""); // Reset message
+        try {
+          const response = await axios.get<ApiResponse>(
+            `/api/check-username?username=${debouncedUsername}`
+          );
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    checkUsernameUnique();
+  }, [debouncedUsername]);
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-
-  return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500" onClick={() => signIn()}>
-        Sign in
-      </button>
-    </>
-  );
+  return <div>Sign In Page</div>;
 }
+
+export default SignIn;
